0d6f939cb468a9969f53764e6fe831ed768f58e7
omar <omarals.com@gmail.com>
2022-03-05 19:37:37 +0300

    Calculator test 1

---
+++ test1/app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText first_number = findViewById(R.id.first_number);
+        EditText second_number = findViewById(R.id.second_number);
+        Button calculate = findViewById(R.id.calculate);
+        TextView total = findViewById(R.id.total);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int n1 = Integer.parseInt(first_number.getText().toString());
+                int n2 = Integer.parseInt(second_number.getText().toString());
+                total.setText(Integer.toString(n1 + n2 ));
+
+
+            }
+        });
+
+    }
+}
